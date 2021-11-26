1e03a889de3b6b191c2819d37cc513a0a3c4ed9e
Gouthami Kolla <89610131+gouthamikolla@users.noreply.github.com>
2021-11-25 20:37:14 -0500

    updated property listings

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -108,11 +108,60 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
+  /*const limitedProperties = {};
   for (let i = 1; i <= limit; i++) {
     limitedProperties[i] = properties[i];
   }
   return Promise.resolve(limitedProperties);
+  */
+  const queryParams = [];
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id `;
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += ` WHERE city LIKE $${queryParams.length}`;
+  }
+  
+  if (options.minimum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night);
+    if (queryParams.length > 0)
+      queryString += ` AND cost_per_night >= $${queryParams.length}`;
+    else
+      queryString += ` WHERE cost_per_night >= $${queryParams.length}`;
+  }
+  if (options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night);
+    if (queryParams.length > 0)
+      queryString += ` AND cost_per_night <= $${queryParams.length}`;
+    else
+      queryString += ` WHERE cost_per_night <= $${queryParams.length}`;
+  }
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    if (queryParams.length > 0)
+      queryString += ` AND rating >= $${queryParams.length}`;
+    else
+      queryString += ` WHERE rating >= $${queryParams.length}`;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+  
+  console.log(queryString, queryParams);
+
+  return pool.query(queryString, queryParams)
+    .then((result) => result.rows)
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 exports.getAllProperties = getAllProperties;
 
