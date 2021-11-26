b41ee7fdcdee1864e3067bb24ef108435a176b84
Gouthami Kolla <89610131+gouthamikolla@users.noreply.github.com>
2021-11-25 20:26:20 -0500

    updated getAllReservations func

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -82,8 +82,21 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  //return getAllProperties(null, 2);
+  return pool
+    .query(`SELECT properties.*
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date <> now()::date
+  AND reservations.start_date <> now()::date
+  LIMIT $2;`, [guest_id , limit])
+    .then((result) => result.rows)
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
+
 exports.getAllReservations = getAllReservations;
 
 /// Properties
